dda0422367cd2a5bfbc0db506f6855bde3c1e824
Levon Arshakyan <lyov.arsh2006@gmail.com>
2020-02-13 20:05:59 +0400

    v2

--- view/layout/header.php
+++ view/layout/header.php
@@ -3,6 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../../public/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <link rel="stylesheet" href="../../public/css/materialize.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -11,15 +21,29 @@
     <title>Document</title>
 </head>
 <body>
-<nav>
-    <div class="nav-wrapper logotype">
-      <a href="#" class="brand-logo logotype">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="#">Sass</a></li>
-        <li><a href="#">Components</a></li>
-        <li><a href="#">JavaScript</a></li>
-      </ul>
+  <nav>
+    <div class='logotp'></div> 
+         <div id="bigpgdiv">
+      <li class="pagesbig pgbighome">Home</li>
+      <li class="pagesbig">Registration</li>
+      <li class="pagesbig">Settings</li>
     </div>
+  <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+       
+      </div>
+    </li>
+    <div class="pagesn">
+      <li class="pages">Home</li>
+      <li class="pages">Registration</li>
+      <li class="pages">Settings</li>
+      <li class="pages">About</li>
+    </div>
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons"><img id='menuimg' src="../../public/images/menu.png" alt="">
+  </i></a>  
+ 
   </nav>
   <section class='row section'>
     <div class='col s5'>
