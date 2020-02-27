819ef23f550ccbd42c9718f69274d82363857b5b
Levon Arshakyan <lyov.arsh2006@gmail.com>
2020-02-27 17:05:46 +0400

    v5

--- view/layout/header.php
+++ view/layout/header.php
@@ -50,22 +50,30 @@
   </nav>
   <section class='row section'>
     <div class='col s5'>
-      <div class="lala">
+      <div class="mthdiv">
       <h1 class='gameicon 'id="math" onclick="math">MATH</h3>
           <div class='icons mth topicon'>
         </div>
       </div>
-      <div class='icons mem botticon'>
-        <h1 class='gameicon'id="memor" onclick="memory">MEMORY</h3>
+      <div class='memdiv'>
+          <h1 class='gameicon'id="memor" onclick="memory">MEMORY</h3>
+          <div class='icons mem botticon'>
+       
+          </div>
       </div>
     </div>
     <div class='col s5'>
-      <div class='icons logic topicon'>
-        <h1 class='gameicon'id="logic" onclick="logic">LOGIC</h3>
+      <div class='logdiv'>
+          <h1 class='gameicon'id="logic" onclick="logic">LOGIC</h3>
+          <div class='icons logic topicon'>
+          </div>
       </div>
-      <div class='icons att botticon'>
-        <h1 class='gameicon'id="attent" onclick="attent">ATTENT</h3>
+      <div class='attdiv'>
+          <h1 class='gameicon'id="attent" onclick="attent">ATTENT</h3>
+          <div class='icons att botticon'>
+          
       </div>
+        </div>
     </div>
     
     
