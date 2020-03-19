129c8c772d0052f11999a522618149271550f3d9
Gor06 <gormargaryan0615@gmail.com>
2020-03-19 18:47:59 +0400

    xsx

--- controller/request.php
+++ controller/request.php
@@ -1,16 +1,30 @@
 <?php
-
+require "../admin/models/db.php";
+require "../models/model.php";
 class request extends model{
-    public function __construct(){
-        $header=get_headers('./request.php');
-        
+    public function __construct()
+    {
+        $for = "for";
+        $regist = "regist";
+        foreach (getallheaders() as $for => $regist) { 
+        echo "$for : $regist <br>"; 
     }
+    }  
     public function login(){
         $request=$_POST['request'];
+
+        print_r( $request);
         explode(" ",$request);
         $sql="SELECT * FROM mechayq_dp WHERE (Username='$request[Username]' OR Email='$request[email]') AND Password='$request[Password]'";
         echo (parent::query($sql))?"ok":"fail";
     }
-}
+    public function regist()
+    { 
+      $_POST(request);
+      parent::setUser();
+    }
 
+}
+$request = new request;
+$request 
 ?>
--- public/js/login.js
+++ public/js/login.js
@@ -1,45 +1,45 @@
-console.log("dfgh");
-sign_up.addEventListener("click", function () {
-  window.location.href = "registration";
-});
-signup_page.addEventListener("click", function () {
-  window.location.href = "registration";
-});
-  login_button.addEventListener("click", function() {
-    event.preventDefault();
-    let obj={};
-    let email_valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let username_vaild = /(([\-\w] +) \.?){ 4, 10 } [a - zA - Z0 - 9]$/;
-    if (email_valid.test(username.value)) {
-        let obj={
-          email: username.value,
-          password: password.value
-        }
+// console.log("dfgh");
+// sign_up.addEventListener("click", function () {
+//   window.location.href = "registration";
+// });
+// signup_page.addEventListener("click", function () {
+//   window.location.href = "registration";
+// });
+login_button.addEventListener("click", function () {
+  event.preventDefault();
+  let obj = {};
+  let email_valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let username_vaild = /(([\-\w] +) \.?){ 4, 10 } [a - zA - Z0 - 9]$/;
+  if (email_valid.test(username.value)) {
+    let obj = {
+      email: username.value,
+      password: password.value
     }
-    else if(username_valid.test(username.value) && username.value.length<12) {
-      let obj = {
+  }
+  else if (username_valid.test(username.value) && username.value.length < 12) {
+    let obj = {
       username: username.value,
       password: password.value
-      }
     }
-    let request=JSON.stringify(obj);
-    $.ajax({
-      url: "./controller/request.php",
-      method: "POST",
-      data: request,
-      headers: {
-        'Content-Type': 'application/json',
-        "for": "login"
-      },
-      success: function (res) {
-        if(res){
-          console.log(res);
-        }else{
-          username.style.border="red";
-          password.style.border="red";
-        }
-          
-        }
-    });
+  }
+  let request = JSON.stringify(obj);
+  $.ajax({
+    url: "./controller/request.php",
+    method: "POST",
+    data: request,
+    headers: {
+      'Content-Type': 'application/json',
+      "for": "login"
+    },
+    success: function (res) {
+      if (res) {
+        console.log(res);
+      } else {
+        username.style.border = "red";
+        password.style.border = "red";
+      }
+
+    }
   });
+});
 
