1560898b50312c7269323fc3447f9a774e76025e
Elen <elen.v.grigoryan@gmail.com>
2020-03-21 19:10:38 +0400

    front 21.03

--- public/js/login.js
+++ public/js/login.js
@@ -5,20 +5,28 @@ sign_up.addEventListener("click", function () {
 signup_page.addEventListener("click", function () {
   window.location.href = "registration";
 });
-  login_button.addEventListener("click", function() {
+login_button.addEventListener("click", function() {
     event.preventDefault();
-    // let obj={};
-    let email_valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let obj={};
     let username_vaild = /(([\-\w] +) \.?){ 4, 10 } [a - zA - Z0 - 9]$/;
- 
-  //  if(username_valid.test(username.value) && username.value.length<12) {
-    let obj = {
-      username: username.value,
-      password: password.value
+    let email_valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email_valid.test(username.value)) {
+      obj ={
+        email: username.value
+      }
     }
-    let queryString = Object.keys(obj).map(key => key + '=' + obj[key]).join('&');
-     // let request=JSON.stringify(obj);
-    console.log(username.value);
+    else if (username_vaild.test(username.value)) {
+      obj = {
+        username: username.value
+      }
+    } 
+    let password_valid = /(?=^.{6,}$)((?=.*\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))^.*/;
+    if (password_valid.test(password.value)) {
+      obj = {
+        ...obj,
+        password: password.value
+    };
+  }
     $.ajax({
       url: "./controller/request.php",
       method: "GET",
